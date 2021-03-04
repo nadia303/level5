c58b81ed5a40950fe33f39ebe930fa1c61f127ad
nadia303 <76038344+nadia303@users.noreply.github.com>
2021-03-04 13:32:23 +0200

    fixed timer bug, added husky

--- Dist/bundle.js
+++ Dist/bundle.js
@@ -1,79 +1,114 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
-const moment = require("moment");
+const moment = require('moment');
 
-let timerValue = document.getElementById("timer_value");
-let info = document.getElementById("timer_hint");
-let plus = document.getElementById("plus");
-let minus = document.getElementById("minus");
-let start = document.getElementById("start");
+const timerValue = document.getElementById('timer_value');
+const info = document.getElementById('timer_hint');
+//plus button
+const plus = document.getElementById('plus');
+//minus button
+const minus = document.getElementById('minus');
+//start button
+const start = document.getElementById('start');
+//variable to save the number of minutes to count
 let minutesNumber = 5;
 
+/**
+ * Increases the number of minutes be clicking on plus button
+ */
 plus.onclick = () => {
-    let value = timerValue.innerHTML;
-    minutesNumber = parseInt(value) + 1;
-    timerValue.innerHTML = minutesNumber;
-
+  const value = timerValue.innerHTML;
+  minutesNumber = parseInt(value) + 1;
+  timerValue.innerHTML = minutesNumber;
 };
 
+/**
+ * Decreases the number of minutes by clicking on minus button
+ */
 minus.onclick = () => {
-    let value = timerValue.innerHTML;
-    minutesNumber = parseInt(value);
-    if (minutesNumber < 1) {
-        timerValue.innerHTML = 0;
-    } else {
-        minutesNumber--;
-        timerValue.innerHTML = minutesNumber;
-    }
+  const value = timerValue.innerHTML;
+  minutesNumber = parseInt(value);
+  if (minutesNumber < 1) {
+    timerValue.innerHTML = 0;
+  } else {
+    minutesNumber--;
+    timerValue.innerHTML = minutesNumber;
+  }
 };
 
-
+/**
+ * Starts the timer by clicking on start button
+ */
 start.onclick = () => {
-    let container = document.getElementById('timer_container');
-    container.classList.add('center');
-    start.classList.add("hidden");
-    plus.classList.add("hidden");
-    minus.classList.add("hidden");
-    info.innerText = 'Time left:';
-    let duration = moment.duration(minutesNumber, "minutes");
+  const container = document.getElementById('timer_container');
+  container.classList.add('center');
+  //hides unneeded elements
+  start.classList.add('hidden');
+  plus.classList.add('hidden');
+  minus.classList.add('hidden');
+  info.innerText = 'Time left:';
+  //the actual duration
+  let duration = moment.duration(minutesNumber, 'minutes');
 
-//corrects the number under 10
-    function checkDigit(number) {
-        let correctedNumber = number.toString()
-        if (correctedNumber.length < 2) {
-            return "0" + correctedNumber;
-        }
-        return number;
+  /**
+   * Corrects the number under 10
+   * @param number is the number to correct
+   * @returns the corrected number
+   */
+  function checkDigit(number) {
+    const correctedNumber = number.toString();
+    if (correctedNumber.length < 2) {
+      return `0${correctedNumber}`;
     }
+    return number;
+  }
 
-    function timer() {
-        duration = moment.duration(duration.asSeconds() - 1, 'seconds');
-        let minutes = duration.minutes();
-        let seconds = duration.seconds();
-
-        if (minutes == 0 && seconds == 0) {
-            myStopFunction();
-        }
-        timerValue.innerHTML = duration.minutes() + ':' + checkDigit(duration.seconds());
+  /**
+   * Shows the countdown
+   */
+  function timer() {
+    //in order not to count under 0
+    if (minutesNumber>0){
+      duration = moment.duration(duration.asSeconds() - 1, 'seconds');
+      const minutes = duration.minutes();
+      const seconds = duration.seconds();
+      if (minutes === 0 && seconds === 0) {
+        myStopFunction();
+      }
+      timerValue.innerHTML = `${checkDigit(duration.minutes())}:${checkDigit(
+        duration.seconds()
+      )}`;
     }
+    else addButton();
+  }
 
-    function myStopFunction() {
-        clearInterval(timerId);
-        addButton();
-    }
+  /**
+   * Stops the timer when it reaches 0
+   */
+  function myStopFunction() {
+    clearInterval(timerId);
+    addButton();
+  }
 
-    function addButton(){
-        start.style.display = 'block';
-        start.innerText = 'Restart';
-    }
+  /**
+   * Adds the restart button in order to restart the timer
+   */
+  function addButton() {
+    start.style.display = 'block';
+    start.innerText = 'Restart';
+  }
 
-    start.onclick = () =>{
-        window.location.reload(false);
-    }
+  /**
+   * Reloads the page in order to restart the timer
+   */
+  start.onclick = () => {
+    window.location.reload(false);
+  };
 
-    let timerId = setInterval(() => {
-        timer()
-    }, 1000);
-}
+  let timerId = setInterval(() => {
+    timer();
+  }, 1000);
+};
+
 },{"moment":2}],2:[function(require,module,exports){
 //! moment.js
 //! version : 2.29.1
